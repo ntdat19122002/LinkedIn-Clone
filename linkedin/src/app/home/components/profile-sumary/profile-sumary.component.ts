2a35a25e5038e2562fe778e06db39724e66e0d15
ntdat19122002 <71707402+ntdat19122002@users.noreply.github.com>
2022-04-25 17:27:52 +0700

    25/4

---
+++ linkedin/src/app/home/components/profile-sumary/profile-sumary.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-sumary',
+  templateUrl: './profile-sumary.component.html',
+  styleUrls: ['./profile-sumary.component.scss'],
+})
+export class ProfileSumaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ linkedin/src/app/home/home.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileSumaryComponent } from './components/profile-sumary/profile-sumary.component';
+import { StartPostComponent } from './components/start-post/start-post.component';
+import { AdvertisingComponent } from './components/advertising/advertising.component';
+import { ModalComponent } from './components/start-post/modal/modal.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [
+    HomePage,
+    HeaderComponent,
+    ProfileSumaryComponent,
+    StartPostComponent,
+    AdvertisingComponent,
+    ModalComponent
+  ]
+})
+export class HomePageModule {}
